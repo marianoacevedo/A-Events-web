9fd8e7937f0e9087e8e06c2199eab685cb817a04
Mariano Acevedo <76134919+marianoacevedo@users.noreply.github.com>
2023-03-07 22:44:16 -0300

    Correcciones (#15)
    * add style check and buscador
    
    * add style index, events, details
    
    * add style contact
    
    * add style stats
    
    * Arreglo checkbox
    
    * mod style navbar
    
    * add style detail and add check events
    
    * add card event
    
    * add style card event
    
    * add card upcoming event
    
    * add past events
    
    * Mejora check
    
    * add favicon
    
    * capture data contact
    
    * Capture data check
    
    * Capture data search
    
    * Correcciones

--- Assets/js/data_ae_1.js
+++ Assets/js/data_ae_1.js
@@ -171,27 +171,3 @@ let data = {
       }
     ]
   }
-
-
-let cards = []
-
-function eventCard ()  {
-  for (let event of data.events) {
-      let card =`<article class="card-articulo">
-                 <img src="${event.image}" class="img" alt="cinema">
-                 <h3>${event.name}</h3>
-                 <p class="card-descripion">${event.description}</p>
-                 <div class="card-precio-buttom">
-                 <p class="precio">$${event.price}</p>
-                 <button><a href="details.html">Ver Mas</a></button>
-                 </div>
-                 </article>`
-      cards.push(card)
-  }
-  return cards.join('')
-}
-
-let cardEvent = document.getElementById("cardEvent")
-cardEvent.innerHTML = eventCard()
-
-
