5768d18d87386d6ebdd174462f9e2d3d5a3faef1
Mariano Acevedo <76134919+marianoacevedo@users.noreply.github.com>
2023-03-09 01:43:42 -0300

    Add api home, events, past events, details (#18)
    * add style check and buscador
    
    * add style index, events, details
    
    * add style contact
    
    * add style stats
    
    * Arreglo checkbox
    
    * mod style navbar
    
    * add style detail and add check events
    
    * add card event
    
    * add style card event
    
    * add card upcoming event
    
    * add past events
    
    * Mejora check
    
    * add favicon
    
    * capture data contact
    
    * Capture data check
    
    * Capture data search
    
    * Correcciones
    
    * Add details
    
    * Add search
    
    * Add api home, events, past events, details

--- Assets/js/details.js
+++ Assets/js/details.js
@@ -1,18 +1,30 @@
-let query = location.search
+let urlApi = "https://mh-h0bh.onrender.com/api/amazing";
 
-let params = new URLSearchParams(query)
+let detail = document.getElementById("detail");
 
-let id = params.get('id')
+let query = location.search;
 
-let card = data.events.find(detail => detail._id == id)
+let params = new URLSearchParams(query);
 
-function detailCard(){
-    return `<div class="img-detail"><img src="${card.image}" class="img-medida" alt="cinema"></div>
-            <div class="box-description-detail">
-            <h3 class="detail-h3">${card.name}</h3>
-            <p class="card-descripion-detail">${card.description}</p>
-            </div>`
+let id = params.get("id");
+
+async function eventCardApi() {
+  try {
+    let response = await fetch(urlApi);
+
+    let data = await response.json();
+
+    let card = data.response.find((detail) => detail.id == id);
+
+    let cardDetail = `<div class="img-detail"><img src="${card.image}" class="img-medida" alt="cinema"></div>
+                      <div class="box-description-detail">
+                      <h3 class="detail-h3">${card.name}</h3>
+                      <p class="card-descripion-detail">${card.description}</p>
+                      </div>`;
+
+    detail.innerHTML = cardDetail;
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-let detail = document.getElementById("detail")
-detail.innerHTML = detailCard()
+eventCardApi();
--- Assets/js/home.js
+++ Assets/js/home.js
@@ -1,14 +1,18 @@
+let urlApi = "https://mh-h0bh.onrender.com/api/amazing"
+
+let cardEvent = document.getElementById("cardEvent")
+
 let cards = []
 
-function eventCard ()  {
-  for (let event of data.events) {
+function eventCard (response)  {
+  for (let event of response) {
       let card =`<article class="card-articulo">
                  <img src="${event.image}" class="img" alt="cinema">
                  <h3>${event.name}</h3>
                  <p class="card-descripion">${event.description}</p>
                  <div class="card-precio-buttom">
                  <p class="precio">$${event.price}</p>
-                 <button><a href="details.html?id=${event._id}">Ver Mas</a></button>
+                 <button><a href="details.html?id=${event.id}">Ver Mas</a></button>
                  </div>
                  </article>`
       cards.push(card)
@@ -16,5 +20,16 @@ function eventCard ()  {
   return cards.join('')
 }
 
-let cardEvent = document.getElementById("cardEvent")
-cardEvent.innerHTML = eventCard()
+async function eventCardApi(){
+  try{
+    let response = await fetch(urlApi)
+    
+    let data = await response.json()
+    
+    cardEvent.innerHTML = eventCard(data.response)
+  }
+  catch(error){
+    console.log(error)
+  }
+}
+eventCardApi()
